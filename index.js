9f0ff42902f8cc709e3adabc452ed9f9ef227d02
DeepakGupta4 <dkguptak9369.com>
2026-02-09 17:31:19 +0530

    fixing bugs

--- index.js
+++ index.js
@@ -28,7 +28,15 @@ dotenv.config();
 const app = express();
 
 // ------------------ MIDDLEWARE ------------------
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}));
+
+app.options("*", cors());
+
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
