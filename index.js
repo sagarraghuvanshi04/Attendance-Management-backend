43b6e448a575da4d7d2bd23ca51048eb4e5c043d
DeepakGupta4 <dkguptak9369.com>
2026-02-09 17:35:15 +0530

    bugs fix

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.use(cors({
   allowedHeaders: ["Content-Type", "Authorization"],
 }));
 
-app.options("*", cors());
+// app.options("*", cors());
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
