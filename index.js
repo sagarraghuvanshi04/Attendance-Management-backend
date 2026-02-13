cdf0f130dfe1f674da2070009fa33a0278878319
DeepakGupta4 <dkguptak9369.com>
2026-02-13 13:40:51 +0530

    Fix CORS: Remove duplicate headers middleware

--- index.js
+++ index.js
@@ -38,17 +38,6 @@ app.use(
   })
 );
 
-// Explicit CORS headers
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
-  next();
-});
-
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
